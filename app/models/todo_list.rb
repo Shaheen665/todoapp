8b1d9b589777c7fe5446e7e96cb10d3553bb7dfc
Shaheen665 <ishaheen523@gmail.com>
2019-09-18 13:45:58 +0500

    changes

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,4 +1,8 @@
 class TodoList < ApplicationRecord
+
   has_many :todo_items, dependent: :destroy
   validates_presence_of :title, :description
+  validates :user_id, presence: true
+  belongs_to :user
+
 end
